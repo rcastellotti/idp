44f0ec1ca5f567bd0a1a008bd7e1e137b511c1ca
Roberto Castellotti <r.castellotti@tum.de>
2023-03-16 10:50:36 +0100

    Update run_traceroutes.sh

--- run_traceroutes.sh
+++ run_traceroutes.sh
@@ -5,6 +5,12 @@ date=$(date +"%Y-%m-%dT%H:%M:%S")
 mkdir -p traceroutes/$date
 for target in "${targets[@]}"
 do
-    traceroute -i enp3s0 $target >> traceroutes/${date}/traceroute_default_${target}_${date}.txt
-    traceroute -i enp1s0f2 $target >> traceroutes/${date}/traceroute_starlink_${target}_${date}.txt
+    traceroute -A -i enp3s0 $target >> traceroutes/${date}/traceroute_default_${target}_${date}.txt
+    traceroute -A -i enp3s0 -T $target >> traceroutes/${date}/traceroute_default_tcp_${target}_${date}.txt
+    traceroute -A -i enp3s0 -4 $target >> traceroutes/${date}/traceroute_default_ipv4_${target}_${date}.txt
+    traceroute -A -i enp3s0 -6 $target >> traceroutes/${date}/traceroute_default_ipv6_${target}_${date}.txt
+    traceroute -A -i enp1s0f2 $target >> traceroutes/${date}/traceroute_starlink_${target}_${date}.txt
+    traceroute -A -i enp1s0f2 -T $target >> traceroutes/${date}/traceroute_starlink_tcp_${target}_${date}.txt
+    traceroute -A -i enp1s0f2 -4 $target >> traceroutes/${date}/traceroute_starlink_ipv4_${target}_${date}.txt
+    traceroute -A -i enp1s0f2 -6 $target >> traceroutes/${date}/traceroute_starlink_ipv6_${target}_${date}.txt
 done
