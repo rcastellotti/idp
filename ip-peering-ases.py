5a7496cb346ae3aa1af2d21ea74e84d730a5965b
Roberto Castellotti <me@rcastellotti.dev>
2023-04-17 17:07:22 +0200

    switched to csv

--- ip-peering-ases.py
+++ ip-peering-ases.py
@@ -1,11 +1,14 @@
 # this data comes from https://publicdata.caida.org/datasets/as-relationships/serial-1/20230401.as-rel.txt.bz2
 # produces a file containing a list of hostnames belonging to each as
+# number of prefixes is configurable
+
+# TODO: download the file and clear it automatically
 
 # sample usage:
 #   python3 ip-peering-ases.py \
-#       --ases 10 --prefix 5 --ips 5 \
+#       --ases 10 --prefix 5  \
 #       -i ../idp-castellotti-data/20230401.as-rel.txt \
-#       -o ../idp-castellotti-data/ip-peering-ases.json
+#       -o ../idp-castellotti-data/ip-peering-ases.csv
 
 import csv
 import argparse
@@ -16,11 +19,12 @@ import random
 import socket
 from tqdm import tqdm
 import logging
-from pygments import highlight, lexers, formatters
 
 csv.register_dialect("piper", delimiter="|", quoting=csv.QUOTE_NONE)
 
-parser = argparse.ArgumentParser(prog="parse", description="caidastuff helper")
+parser = argparse.ArgumentParser(
+    prog="parse", description="extract ips from ases peering with spacex"
+)
 parser.add_argument("--input", "-i", help="input", required=True)
 parser.add_argument("--output", "-o", help="output", required=True)
 parser.add_argument(
@@ -29,18 +33,19 @@ parser.add_argument(
 parser.add_argument(
     "--prefix", "-p", help="number of prefixes", required=True, type=int
 )
-parser.add_argument("--ips", "-j", help="number of ips", required=True, type=int)
 parser.add_argument(
     "--verbose", "-v", help="verbose", action=argparse.BooleanOptionalAction
 )
 args = parser.parse_args()
 if args.verbose:
     logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
 else:
     logging.getLogger().setLevel(logging.INFO)
 
 # getting all ASes peering with SpaceX (AS14593)
-ases=set()
+ases = set()
 with open(args.input, "r") as csvfile:
     for row in csv.reader(csvfile, dialect="piper"):
         if row[2] == "0":
@@ -50,43 +55,41 @@ with open(args.input, "r") as csvfile:
                 ases.add(row[1])
 
 # getting as names, prefixes and random ips from certain ases
-ases_ip = []
 ases_list = random.choices(list(ases), k=args.ases)
+
+with open(args.output, "a") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["asn", "asname", "prefix", "ip", "hostname"])
+
 for _as in ases_list:
-    r = requests.get("https://stat.ripe.net/data/as-names/data.json", params={"resource":_as})
+    r = requests.get(
+        "https://stat.ripe.net/data/as-names/data.json", params={"resource": _as}
+    )
     asname = r.json()["data"]["names"][_as]
-    url = "https://stat.ripe.net/data/announced-prefixes/data.json"
-    params = {"resource": _as, "starttime": "2023-04-01T12:00"}
-    r = requests.get(url, params=params)
+
+    r = requests.get(
+        "https://stat.ripe.net/data/announced-prefixes/data.json",
+        params={"resource": _as, "starttime": "2023-04-01T12:00"},
+    )
     j = r.json()
     prefixes = j["data"]["prefixes"]
-    prefixes = random.choices(prefixes, k=args.prefix)
-    formatted_json = json.dumps(j, indent=4)
-    colorful_json = highlight(
-        formatted_json, lexers.JsonLexer(), formatters.TerminalFormatter()
-    )
-    logging.debug(colorful_json)
+
+    # this is needed because some ases announce just a few prefixes (<5)
+    number_of_prefixes = min(len(prefixes) - 1, args.prefix)
+    prefixes = random.choices(prefixes, k=number_of_prefixes)
     logging.info(f"ASN: {_as}")
-    tmp = dict()
-    tmp["asn"] = _as
-    tmp["as_name"] = asname
-    tmp["ips"]=[]
-    tmp["hostnames"]=[]
-    logging.debug(prefixes)
 
     for prefix in tqdm(prefixes):
-        p = prefix["prefix"]
-        if ":" not in p:
-            logging.debug(f"network: {p}")
-            ips = [str(ip) for ip in IPv4Network(p)]
-            ips = random.choices(ips, k=args.ips)
-            logging.debug(f"random ips: {ips}")
-            hostnames = [socket.getnameinfo((ip, 0), 0)[0] for ip in ips]
-            logging.debug(f"hostnames: {hostnames}")
-            tmp["ips"].append(ips)
-            tmp["hostnames"].append(hostnames)
-    ases_ip.append(tmp)
-
-with open(args.output, "w+") as f:
-    json.dump(ases_ip, f,indent=4)
-
+        prefix = prefix["prefix"]
+        if ":" not in prefix:
+            logging.debug(f"network: {prefix}")
+            ips = [str(ip) for ip in IPv4Network(prefix)]
+            ip = random.choice(ips)
+            try:
+                hostname = socket.getnameinfo((ip, 0), 0)[0]
+            except:
+                pass
+            logging.debug(f"hostname: {hostname}")
+            with open(args.output, "a") as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow([_as, asname, prefix, ip, hostname])
