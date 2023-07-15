6847eb81dbf76e9948d80209e0fd9b4bff77c11f
rcastellotti <me@rcastellotti.dev>
2023-07-15 14:37:03 +0000

    update

--- map.py
+++ map.py
@@ -4,10 +4,12 @@ import time
 import argparse
 import ffmpeg
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 import logging
 import nine981
-from PIL import Image, ImageDraw,ImageFont
+from PIL import Image, ImageDraw
+from concurrent.futures import ThreadPoolExecutor
 
 parser = argparse.ArgumentParser(prog="map")
 parser.add_argument(
@@ -28,41 +30,42 @@ directory=args.directory
 if args.verbose:
     logging.basicConfig(level="INFO")
 
-# logging.info("starting to save maps")
-# for i in range(1):
-#     map = nine981.get_obstruction_map()
-#     os.makedirs(os.path.dirname(directory + "/"), exist_ok=True)
-#     with open(f"{directory}/map-{int(time.time())}.json", "w+") as f:
-#         f.write(map)
-#     time.sleep(1)
+logging.info("starting to save maps")
+for i in range(1):
+    map = nine981.get_obstruction_map()
+    os.makedirs(os.path.dirname(directory + "/"), exist_ok=True)
+    with open(f"{directory}/map-{int(time.time())}.json", "w+") as f:
+        f.write(map)
+    time.sleep(1)
 
-for file in os.listdir(directory):
-    with open(f"{directory}/{file}", "r") as f:
-        logging.info(f"elaborating {file}")
+os.makedirs(os.path.dirname(f"{directory}-viz/"), exist_ok=True)
+
+def process_file(filename):
+    img=os.path.join(directory,filename)
+    with open(img, "r") as f:
+        logging.info(f"elaborating {filename}")
         map = json.load(f)
         map = map["dishGetObstructionMap"]["snr"]
         map = np.array(map).reshape(123, 123)
-        map = np.repeat(np.repeat(map, 10, axis=1), 10, axis=0)
-        plt.imshow(map)
-        os.makedirs(os.path.dirname(f"{directory}-viz/"), exist_ok=True)
-        img=f"{directory}-viz/{file}.png"
+        map = np.repeat(np.repeat(map, 2, axis=1), 2, axis=0)
+        img=f"{directory}-viz/{filename}.png"
+        plt.imshow(map, cmap='viridis')
         plt.axis("off")
-        plt.savefig(img)
-        image=Image.open(img)
-        W, H = image.size
-        # font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoSansMono-Bold.ttf", 15)
-        font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 15)
-        draw = ImageDraw.Draw(image)
-        _, _, w, h = draw.textbbox((0, 0), img)
-        draw.text(((W - w) / 2, (H - h)-150), img, font=font,fill="yellow")
-        image.save(img)
+        plt.text(map.shape[1]-120, map.shape[0]-10, filename, color='yellow', fontsize=10, fontweight='bold')
+        img_path = f"{directory}-viz/{filename}.png"
+        plt.savefig(img_path, bbox_inches="tight")
+        plt.close()
 
+
+
+
+files = os.listdir(directory)
+with ThreadPoolExecutor(max_workers=16) as executor:
+    executor.map(process_file, files)
+    print("All files processed.")
 ffmpeg.input( f"{directory}-viz/*.png", framerate=30, pattern_type="glob").output(
     args.output,
     vcodec="libx264",
     pix_fmt="yuv420p",
     vf="pad=ceil(iw/2)*2:ceil(ih/2)*2",
 ).run()
-
-# we can "sum" the matrices to see how many times we saw a satellite in a certain pixel, that will result in pixel
-# with darker colors and we can visualize it better
