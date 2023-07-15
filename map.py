2963fa9c923be2b6d8563b9a8a4487b399caa3e2
Roberto Castellotti <me@rcastellotti.dev>
2023-07-15 11:00:50 +0200

    removed tests

--- map.py
+++ map.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import logging
 import nine981
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw,ImageFont
 
 parser = argparse.ArgumentParser(prog="map")
 parser.add_argument(
@@ -20,31 +20,25 @@ parser.add_argument(
         "--output", "-o", help="output filename", required=True, type=str
 )
 parser.add_argument(
-        "--reboot", help="reboot the dish", required=True, action=argparse.BooleanOptionalAction
-)
-parser.add_argument(
         "--seconds","-s", help="how many seconds we should collect maps for", required=True, type=int
 )
 args = parser.parse_args()
 
 directory=args.directory
+if args.verbose:
+    logging.basicConfig(level="INFO")
 
-if args.reboot:
-    nine981.reboot()
-    logging.info("rebooted device, i am pretty tired, will slepp for 30s")
-    time.sleep(30)
-
-
-logging.info("starting to save maps")
-for i in range(1):
-    map = nine981.get_obstruction_map()
-    os.makedirs(os.path.dirname(directory + "/"), exist_ok=True)
-    with open(f"{directory}/map-{int(time.time())}.json", "w+") as f:
-        f.write(map)
-    time.sleep(1)
+# logging.info("starting to save maps")
+# for i in range(1):
+#     map = nine981.get_obstruction_map()
+#     os.makedirs(os.path.dirname(directory + "/"), exist_ok=True)
+#     with open(f"{directory}/map-{int(time.time())}.json", "w+") as f:
+#         f.write(map)
+#     time.sleep(1)
 
 for file in os.listdir(directory):
     with open(f"{directory}/{file}", "r") as f:
+        logging.info(f"elaborating {file}")
         map = json.load(f)
         map = map["dishGetObstructionMap"]["snr"]
         map = np.array(map).reshape(123, 123)
@@ -55,8 +49,12 @@ for file in os.listdir(directory):
         plt.axis("off")
         plt.savefig(img)
         image=Image.open(img)
-        draw=ImageDraw.Draw(image)
-        draw.text((220, 400), img,  fill="red")
+        W, H = image.size
+        # font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoSansMono-Bold.ttf", 15)
+        font = ImageFont.truetype("/System/Library/Fonts/Supplemental/Arial Unicode.ttf", 15)
+        draw = ImageDraw.Draw(image)
+        _, _, w, h = draw.textbbox((0, 0), img)
+        draw.text(((W - w) / 2, (H - h)-150), img, font=font,fill="yellow")
         image.save(img)
 
 ffmpeg.input( f"{directory}-viz/*.png", framerate=30, pattern_type="glob").output(
